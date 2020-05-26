4cd03ff2c86ffed81be24114f712f6f00073b574
manksharma <2mayanksharma2@gmail.com>
2020-05-25 17:59:31 -0700

    Initial commmit

---
+++ static/displays.js
@@ -0,0 +1,63 @@
+export { gradual_load_text, instant_display_elements, gradual_display_elements }
+
+var gradual_load_text = function(element, text){
+    console.log('WORKING on');
+    console.log(text);
+    return new Promise(
+        (resolve) => gradual_load_helper(element, text, resolve)
+        //function(resolve){
+        //gradual_load_helper(element,text, resolve);
+    //}
+    )
+}
+
+var gradual_load_helper = function(element, text, resolve){
+    var i = 0;
+    var showfull = function(){
+        setTimeout(function(){
+            i++;
+            if (i < text.length){
+                element.innerText = text.substring(0,i).concat('\u2592') ;
+                showfull();
+            }
+            if (i == text.length){
+                element.innerText = text.substring(0, text.length);
+                resolve();
+            }
+        }, 100);
+    }
+    showfull();
+}
+
+var instant_display_elements = function(elements, new_style){
+    return new Promise(function(resolve){
+        instant_display_elements_helper(elements, new_style, resolve);
+    })
+}
+
+var instant_display_elements_helper = function(elements, new_style, resolve){
+    var i = 0;
+    while (i < elements.length){
+        elements[i].style.display = "initial";
+        i++
+    }
+    resolve();
+}
+
+var gradual_display_elements = function(elements, new_style){
+    return new Promise(function(resolve){
+        gradual_display_elements_helper(elements, new_style, resolve);
+    })
+}
+
+var gradual_display_elements_helper = function(elements, new_style, resolve){
+    var j = 0;
+    var timer = setInterval(function(){
+        elements[j].style.display = new_style;
+        j++;
+        if (j == elements.length){
+            clearInterval(timer);
+            resolve();
+        }
+    }, 200);
+}
---
+++ static/script.js
@@ -0,0 +1,73 @@
+import {gradual_load_text, gradual_display_elements, instant_display_elements} from "./displays.js" 
+
+window.onload = function() {
+
+    var complete_title = "MAYANK_SHARMA$ ls portfolio";
+    var title_element = document.getElementById('title').getElementsByTagName('h1')[0];
+
+    var table_of_contents = document.getElementById('table-of-contents').getElementsByTagName('li');
+
+    var subsection_list = document.getElementsByClassName('subsection');
+    var subsection_titles = []
+    for(var i = 0; i < subsection_list.length; i++){
+        subsection_titles.push(subsection_list[i].getElementsByClassName('subsection-title'));
+    }
+    var subsection_titles_text = ["$cat AboutMe.txt", "$ls Projects", "$ls Current Activities"]
+    
+    var subsection_content = []
+    var subsection_headers = [] 
+    for(var i = 0; i < subsection_titles.length; i++){
+        subsection_headers.push(subsection_titles[i][0].getElementsByTagName('h2')[0]);
+        //console.log(subsection_headers[i]);
+        subsection_content.push(subsection_list[i].getElementsByClassName('subsection-content')[0]);
+    }
+
+    
+    
+
+    var promise_made = gradual_load_text(title_element,complete_title)
+    .then(
+        (elements, new_style) => instant_display_elements(table_of_contents, "block") 
+        )
+    .then(
+        (elements, text) => gradual_load_text(subsection_headers[0], subsection_titles_text[0]) 
+    )
+    .then(
+        () => instant_display_elements([subsection_content[0]], "initial")
+    )
+    .then(
+        (elements, text) => gradual_load_text(subsection_headers[1], subsection_titles_text[1]) 
+    )
+    .then(
+        () => instant_display_elements([subsection_content[1]], "initial")
+    )
+    .then(
+        (elements, text) => gradual_load_text(subsection_headers[2], subsection_titles_text[2]) 
+    )
+    .then(
+        () => instant_display_elements([subsection_content[2]], "initial")
+    )
+
+    //TODO: get this loop to work :(
+    
+    /*for(var k = 0; k < subsection_headers.length; k++){
+        console.log(subsection_titles_text[k]);
+        console.log(typeof subsection_titles_text[k]);
+        console.log(typeof "hello");
+        promise_made = promise_made.then(
+            (elements, text) => gradual_load_text(subsection_headers[k],subsection_titles_text[k])
+            /*function(elements, text) { 
+                console.log('STARTING')
+                console.log(subsection_headers[k]);
+                gradual_load_text(subsection_headers[k], "cat aboutme"); 
+            }
+        )
+    }*/
+
+    //a.then(
+    //    function(elements, new_style) { gradual_display_elements(subsection_content, "initial") }
+    //    );
+
+    //this.gradual_load_text(target_element, complete_title,this.gradual_display_elements, subsection_list);
+
+}
